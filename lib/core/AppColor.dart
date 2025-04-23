1f46392d676d72b18ad8896c598c933de4ed6527
AbdoAnany <abdo10002011@gmail.com>
2025-04-24 00:02:25 +0200

    add assets

--- lib/core/AppColor.dart
+++ lib/core/AppColor.dart
@@ -29,4 +29,12 @@ class AppImages {
   static const String background = 'assets/images/background.png';
   static const String deliveryIcon = 'assets/images/delivery.png';
   static const String delivaryBuke = 'assets/images/delivary_buke.png';
+  static const String arabic = 'assets/images/arabic.png';
+  static const String english = 'assets/images/english.png';
+  static const String ic_circle1 = 'assets/images/ic_circle1.png';
+  static const String ic_circlex2 = 'assets/images/ic_circlex2.png';
+  static const String ic_emptyorder = 'assets/images/ic_emptyorder.png';
+  static const String ic_language = 'assets/images/ic_language.png';
+  static const String man = 'assets/images/man.png';
+  static const String search = 'assets/images/search.png';
 }
